aaada95d241671eda3254cbc66e8051d1e71154e
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-01-07 13:17:27 +0530

    My python Programs

---
+++ pyprogram/Loops/Whileloop.py
@@ -0,0 +1,15 @@
+#start
+#condition
+#increment/decrement
+
+i=1
+while i<=10:
+    print(i,"=","Hello World")
+    i=i+1
+
+print(i)  # th e last value of i is 11
+print()
+a=10
+while a>=1:
+    print(a)
+    a = a-1
