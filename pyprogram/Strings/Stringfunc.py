ed19679c2160743213065c4488b3361774aba6d7
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-01-08 13:40:26 +0530

    Update Stringfunc.py

--- pyprogram/Strings/Stringfunc.py
+++ pyprogram/Strings/Stringfunc.py
@@ -3,18 +3,18 @@
 
 #Lower
 z = "Make You Strong"
-l = z.lower()
+l = z.lower()  # it will print small leter of characters
 print(l)
 #Upper
-u = z.upper()
+u = z.upper()  # it will print Upper leter of all characters
 print(u)
 
 # title
-t = z.title()
+t = z.title()  # it will print title leter  characters
 print(t)
 
 # cap
-c = z.capitalize()
+c = z.capitalize()  # it will do first leter of character in capital leter
 print(c)
 
 
