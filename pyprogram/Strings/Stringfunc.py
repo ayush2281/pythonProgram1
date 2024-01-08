ec8246003f57fbdc5aaf9115c62b6d15906b04a1
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-01-08 13:37:37 +0530

    My python Programs

---
+++ pyprogram/Strings/Stringfunc.py
@@ -0,0 +1,21 @@
+# String Function
+# lower(), upper() , title(), capatalize()
+
+#Lower
+z = "Make You Strong"
+l = z.lower()
+print(l)
+#Upper
+u = z.upper()
+print(u)
+
+# title
+t = z.title()
+print(t)
+
+# cap
+c = z.capitalize()
+print(c)
+
+
+
