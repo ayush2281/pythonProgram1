8fcac90f38f9f01c242a60bd9595ce730a091f7c
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-01-08 15:07:35 +0530

    My python Programs

---
+++ pyprogram/Strings/Stringfunc2.py
@@ -0,0 +1,24 @@
+                        # Strings Function
+   # find() , index() , isalpha() ,  isdigit() , isalnum()
+
+#find
+# in the find if we put non given value it will give you answer in -1
+z = "Make You Strong"
+print(z.find('z'))  # OT  -> 3
+print(z.find('o',2))  # the mean of 2 is start from 2 index
+
+#index
+#  in the index if we put non given value it will give you error
+# print(z.index('z'))
+
+#isalpha --> isalpha means all alphabates
+a = "Anonymous123"
+print(a.isalpha()) # output : false because a is not including only alpha charcter it is also number
+
+# isdigit() it include only number
+n = "123"
+print(n.isdigit())  # true
+# isalnum --> it include both alphates and number but dont use special char like @,#,$,&
+r = "Anonymous126"
+print(r.isalnum())
+
