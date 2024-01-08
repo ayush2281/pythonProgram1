85eabcdda1cd2e3f16bf0d0ec50990c17b940b3c
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-01-08 13:28:01 +0530

    My python Programs

---
+++ pyprogram/Strings/iteration.py
@@ -0,0 +1,14 @@
+# You can find String iteration using for loop
+z = "Make You Strong"
+# z = z[-1::-1]
+t = len(z)  # len will give the length characters and it is predefined
+print(t)
+for a in range(t): #15
+     print(z[a])
+
+print()
+
+    # for reverse
+
+for a in range(t-1,-1,-1): #15
+    print(z[a])
---
+++ pyprogram/Strings/iteration2.py
@@ -0,0 +1,11 @@
+# Without len
+z = "Make You Strong"
+for a in z:
+  print(a)
+print()
+# for reverse
+z = "Make You Strong"
+z = z[-1::-1]
+
+for a in z:
+  print(a)
