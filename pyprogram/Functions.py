62afa2e74c7cf68500608c0c9855639424e27934
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-01-09 20:11:39 +0530

    My python Programs

---
+++ pyprogram/Functions.py
@@ -0,0 +1,10 @@
+# ASCII Value is start from A To Z and ascii value of A To Z 65 to  90
+# ASCII Value is start from a To z   and ascii value of a To z 97 to 122
+# Python chr() and ord()
+# chr()
+y = chr(65)
+print(type(y),y)
+
+#the ord() func
+z = ord('A')
+print(type(z),z)
