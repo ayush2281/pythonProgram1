a82633e41fb04ac41eae619d892a73d936c20c67
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-01-11 12:54:28 +0530

    My python Programs

---
+++ pyprogram/Strings/stringFormat.py
@@ -0,0 +1,2 @@
+# the format() method formats the specified value(s) and insert the string's placeholder.
+#the placeholder is defined using curly brackets {}. read more about the placeholder in the placeholder section below.
