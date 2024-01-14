d2125f8b9c96ab80d6e3705064c202c702e8e741
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-01-14 09:24:37 +0530

    My python Programs

--- pyprogram/list/list-iteration.py
+++ pyprogram/list/list-iteration.py
@@ -1,13 +1,30 @@
 # LIST ITERATION
 
 l=[2,3,56,98,2,10]
-a = len(l) # length of l
-print(a)
+t = len(l) # length of l
+print(t)
 
+#it will index no start from reverse
+for a in range(t-1,-1,-1):
+    print(a)
+
+#reverse
+for a in range(t-1,-1,-1):
+    print(l[a])
+
+
+
+
+
+
+#method1
 # for n in range(a):
     # print(l[n])
 
+#method2
+# for a in l:
+    # print(a)
 
-for a in l:
-    print(a)
+
+
 
