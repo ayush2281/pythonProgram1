1f891220eb792d6ece81a310902067dbc1e242c6
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-01-14 22:41:11 +0530

    My python Programs

---
+++ pyprogram/list/listfunc3.py
@@ -0,0 +1,28 @@
+#count() : it will count how much time use same number in list.
+# l =[5,3,7,7,2,5]
+# c=l.count(5)
+# print(c) # output = 2
+
+# max() : it will help to get heighest value in list
+# l2 =[10,20,30,60]
+# m = max(l2)
+# print(m)
+
+# a =["Hello",'word']
+# m=max(a)
+# print(m)
+
+# min() : it will help to get lowest value in list
+
+l3 =[10,20,30,60]
+v = min(l3)
+print(v)
+
+a =["Hello",'word']
+m=min(a)
+print(m)
+
+# 4 sort()
+l4=[10,20,30,50]
+   l4.sort()
+   print(l4)
