8881436d579e64c53b3d04227cddfdd92c020db3
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-01-21 10:29:02 +0530

    My python Programs

---
+++ pyprogram/Dictionary.py
@@ -0,0 +1,17 @@
+#directory
+# dictionary is an unordered collection of key-values pairs.
+#  in python , dictionaries are defined  within breaces{} with each items being pair in the form key:values
+
+
+d = {
+    'name': 'python',
+    'fees': 8000,
+    'duration': '2Months'
+}
+
+# print(d['name'])
+# print(type(d))
+# print(d['fees'])
+
+for n in d:
+    print(d[n])
--- pyprogram/Queue.py
+++ pyprogram/Queue.py
@@ -1,43 +1,43 @@
 #the Queue is linear data type
 #stores items in first out(FIFO) manner
 
-l=[]
-while True:
-    c=int(input('''
-       1 Push Element
-       2 Pop first Element
-       3 front Element
-       4 last Element
-       5 display 
-       6 exit    
-       '''))
-
-    if c==1:
-        n=input("Enter the value")
-        l.append(n)
-        print(l)
-    elif c==2:
-        if len(l)==0:
-            print("Empty Queue")
-        else:
-            del l[0]
-            print(l)
-
-    elif c==3:
-        if len(l)==0:
-            print("Empty Queue")
-        else:
-            print("first Queue element",l[0])
-    elif c==4:
-        if len(l)==0:
-            print("Empty Queue")
-        else:
-            print("last Queue element",l[-1])
-    elif c==5:
-        print("display Queue",l)
-
-    elif c==6:
-        break;
-    else:
-        print("invallid opr")
-
+# l=[]
+# while True:
+#     c=int(input('''
+#        1 Push Element
+#        2 Pop first Element
+#        3 front Element
+#        4 last Element
+#        5 display
+#        6 exit
+#        '''))
+#
+#     if c==1:
+#         n=input("Enter the value")
+#         l.append(n)
+#         print(l)
+#     elif c==2:
+#         if len(l)==0:
+#             print("Empty Queue")
+#         else:
+#             del l[0]
+#             print(l)
+#
+#     elif c==3:
+#         if len(l)==0:
+#             print("Empty Queue")
+#         else:
+#             print("first Queue element",l[0])
+#     elif c==4:
+#         if len(l)==0:
+#             print("Empty Queue")
+#         else:
+#             print("last Queue element",l[-1])
+#     elif c==5:
+#         print("display Queue",l)
+#
+#     elif c==6:
+#         break;
+#     else:
+#         print("invallid opr")
+#
