2f9a32e113b6f084314dd13bc927c2ca899e5817
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-01-22 22:24:44 +0530

    My python Programs

--- pyprogram/Tupple.py
+++ pyprogram/Tupple.py
@@ -1,5 +1,19 @@
 # Tupple
 # tupple is an ordered sequences of items same as a list
 # it is defined within parenthesis () where items are seprated by commas
+#iterations
 t = (96,3,'hello')
 print(t,type(t))
+a=t[1]
+print(a,type(a))
+
+# method1
+l =len(t)
+for a in range(l):
+    print(t[a])
+# method
+for a in t:
+    print(a)
+
+
+    # Now Come to tupple functions
