ee264fa770736c704d92efeb1c7183622e6bba70
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-01-23 19:41:11 +0530

    My python Programs

---
+++ pyprogram/Sets.py
@@ -0,0 +1,5 @@
+#set
+# is an unordered collection of list
+#Every set element is unique(no duplicates) and must be immutable (can't be changed)
+my_set = {2,3,75,65}
+print(my_set,type(my_set))
--- pyprogram/Tupple.py
+++ pyprogram/Tupple.py
@@ -1,19 +1,22 @@
 # Tupple
 # tupple is an ordered sequences of items same as a list
 # it is defined within parenthesis () where items are seprated by commas
-#tupple iterations
-# t = (96,3,'hello')
-# print(t,type(t))
-# a=t[1]
-# print(a,type(a))
-#
-# # method1
-# l =len(t)
-# for a in range(l):
-#     print(t[a])
-# # method
-# for a in t:
-#     print(a)
+# tupple iterations
+t2 = (96,3,'hello')
+print(t2,type(t2))
+a=t2[1]
+print(a,type(a))
+
+n = t2[0]
+print(n)
+
+# method1
+l =len(t2)
+for a in range(l):
+    print(t2[a])
+# method
+for a in t2:
+    print(a)
 
 
     # Now Come to tupple functions
@@ -40,6 +43,10 @@ print(m2)
 s = sum(t,10) #if you want to add anynumber in number pass(,and any number)
 print(s)
 
+
+#index
+# n = t[0]
+# print(n)
 
 
 
