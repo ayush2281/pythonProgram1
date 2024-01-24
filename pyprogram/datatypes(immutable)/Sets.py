3ef23c690d16fafb0a3db622e3dacc0c28b481e3
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-01-24 13:17:28 +0530

    My python Programs

--- pyprogram/datatypes(immutable)/Sets.py
+++ pyprogram/datatypes(immutable)/Sets.py
@@ -1,5 +1,8 @@
 #set
 # is an unordered collection of list
 #Every set element is unique(no duplicates) and must be immutable (can't be changed)
-my_set = {2,3,75,65}
+my_set = {2,3,75,3,65}
 print(my_set,type(my_set))
+
+for a in my_set:
+    print(a)
