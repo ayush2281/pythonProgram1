11ca4a391c61e414c0331f75841ac0b1ceb049a0
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-01-27 14:13:52 +0530

    My python Programs

--- pyprogram/Modules/date&time.py
+++ pyprogram/Modules/date&time.py
@@ -5,7 +5,7 @@
 # 1st. datetime is module , 2nd. datetime is object datetime ke andr bana hua 3rd.now function is calling
 
 import datetime
-x =datetime.datetime.now()
+x =datetime.datetime.now()#current date time
 print(x)
 
 #the date contain year,month,hour,minute,second,and microsecond
@@ -13,3 +13,21 @@ print(x)
 # Creating date object
 
 print(datetime.datetime(2024,1,26))
+
+       #Python Dates
+# he method is called strftime(), and takes one parameter, format, to specify the format of the returned string:
+# %b Month name, short version Dec
+# %B Month name, full version # December
+# %m  Month as a number 01-12 12
+# %y Year, short version, without century 18
+# %Y Year, full version 2018
+# %H Hour 00-23 17
+# %I Hour 00-12 05
+# %p AM/PM PM
+# %M Minute 00-59  41
+
+
+now = datetime.datetime.now() #current date time
+year = now.strftime("%y")
+# print(now)
+print("year",year)
