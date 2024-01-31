2655a4274ee0df7e460793abf30ff9fda9c0ac4b
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-01-31 22:39:34 +0530

    My python Programs

---
+++ pyprogram/JSON/JSONTOPYTHON.py
@@ -0,0 +1,10 @@
+# Python JSON
+# if you have a json string, you can  parse it by using the json.loads()method.
+
+import json
+d='{"cname":"Python","fees":12000,"duration":"45Days"}'
+x=json.loads(d)
+print(type(x))
+print(x)
+for a in x:
+    print(a,x[a])
