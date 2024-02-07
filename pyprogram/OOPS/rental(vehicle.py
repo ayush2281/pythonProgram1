ed32ebbaa86bc8d180d97f792d4461b1232bbe48
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-02-07 23:28:07 +0530

    My python Programs

--- pyprogram/OOPS/rental(vehicle.py
+++ pyprogram/OOPS/rental(vehicle.py
@@ -3,18 +3,31 @@
 # 2.request a bike foe rent(100R->1qty)
 # 3.Exit
 class BikeShop:
-    def __init__(self,stock): #constructor
-        self.stock=stock
+    def __init__(self,stock): #create constructor
+        self.stock=stock      #Variable
     def displayBike(self):
         print("total Bikes",self.stock)
     def rentForbike(self,q):
         if q<=0:
-            print("Enter the positive value or grator than zero")
-        elif q>self:
-            print("Enter thr the value (less then stock")
+            print("Enter the positive value or greator then zero")
+        elif q>self.stock:
+            print("Enter the value (less then stock")
         else:
-            print("total prices",q*100)
+            print("total prices",q*1000)
             print("total Bikes",self.stock)
 while True:
-    uc=int("in")
+    obj=BikeShop(100)
+    uc=int(input('''
+1 display Stocks
+2 rent a bike
+3 exit
+
+            '''))
+    if uc==1:
+        obj.displayBike()
+    elif uc==2:
+        n=int(input("Enter the QTY.."))
+        obj.rentForbike(n)
+    else:
+        break
 
