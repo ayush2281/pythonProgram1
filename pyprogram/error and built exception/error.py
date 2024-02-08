190a5209cf6214067a7ed406d052e170c4b84f1b
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-02-08 17:45:16 +0530

    My python Programs

---
+++ pyprogram/error and built exception/error.py
@@ -0,0 +1,22 @@
+# these erroe can be broadle classsified into two classes:
+# syntax error
+# logical error(Exception)
+
+
+
+            # syntax error
+# a = 10
+# b=20
+# if a==b
+#     print("NO")
+# else
+#     print("yes")
+
+    # logical error(Exception)
+    a= 10
+b = 20
+if a == b:
+    print("NO")
+else:
+    print("yes")
+print(a/0)
