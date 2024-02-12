757af1ba143ce5689fccd768dccfd4949ebc9789
AyushSinghRajput1299 <134182912+ayushsingh12-cloud@users.noreply.github.com>
2024-02-12 19:03:48 +0530

    My python Programs

--- pyprogram/SQLite/sqliteconnenct.py
+++ pyprogram/SQLite/sqliteconnenct.py
@@ -1,14 +1,12 @@
 import sqlite3
 conn=sqlite3.connect("sqlite.db")
 conn.execute('''
-       Create table student (
-          st_id INT AUTO_INCREMENT PRIMARY KEY
-          st_name VArCHAR(50)
-       
-       
-       
-       
-       )
-
-
-   ''')
+        Create table student (
+            st_id INT AUTO_INCREMENT PRIMARY KEY,
+            st_name VARCHAR(50),
+            st_class VARCHAR(10),
+            st_email VARCHAR(30)
+        )
+      
+    ''')
+conn.close()
